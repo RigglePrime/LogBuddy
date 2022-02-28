8ce228ace5e64c0258713d75aa9aeb3b75445ed4
RigglePrime <27156122+RigglePrime@users.noreply.github.com>
2022-02-28 19:51:39 +0100

    Fixed!

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description='Parses-the-logs')
 
-    parser.add_argument("-v", "--verbose", action="store_true", help="Toggle verbose mode") # TODO: fix option, it gets interpreted as a file
+    parser.add_argument("-v", "--verbose", action="store_true", help="Toggle verbose mode")
     parser.add_argument("-q", "--quiet", "--silent", action="store_false", help="Toggle silent mode")
     parser.add_argument("--version", action="version", version="LogBuddy v0.1")
     parser.add_argument("file", nargs="*", help="One or multiple log files or a single folder containing log files to parse")
