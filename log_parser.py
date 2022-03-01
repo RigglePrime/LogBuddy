08e7e8759109e1d0e4b006ba81a1222c367cbbcf
RigglePrime <27156122+RigglePrime@users.noreply.github.com>
2022-03-01 20:31:41 +0100

    Forgot mecha!

--- log_parser.py
+++ log_parser.py
@@ -278,7 +278,7 @@ class LogFile:
         """
         import requests as req
         # Should be all supported log types as a default. Don't forget to update this list! (you will)
-        if not logs_we_care_about: logs_we_care_about = ["game.txt", "attack.txt", "pda.txt", "silicon.txt", "virus.txt"]
+        if not logs_we_care_about: logs_we_care_about = ["game.txt", "attack.txt", "pda.txt", "silicon.txt", "mecha.txt", "virus.txt"]
         if not link[-1] == "/": link += "/"
         log_collection = LogFile()
         for log_file in logs_we_care_about:
